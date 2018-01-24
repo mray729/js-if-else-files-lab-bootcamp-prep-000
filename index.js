deb207e2935846ae07f23da289cfb2a9a5450a31
Melonie Ray <mray729@gmail.com>
2018-01-24 02:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var name = "Joe"
 var height = "74"
 if (name==="Joe")
 console.log("The name is correct")
-}else if(name==="Susan")
-
+}else {
+}
+if(height==="74")
+}else{
+}
 
 
 // Don't worry about this
