e921b15a9dc27a8acf976f75e9558de1e10a6c97
Melonie Ray <mray729@gmail.com>
2018-01-24 01:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var name = 'Susan';
-var height = "74";
-if (name===Susan){
-  console.log("I am Joe.");
+var name = "Susan"
+var height = "74"
+if (name==="Susan"){
+  console.log("I am Joe.")
 }
 else{
   console.log("Expected the name to be Susan");
